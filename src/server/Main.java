704edf48ea5d63bfcf679a8f9a59b702c42a2a96
Kostya <aktybaeff@yandex.ru>
2023-10-04 16:19:28 +0500

    Initial commit

---
+++ src/server/Main.java
@@ -0,0 +1,10 @@
+package server;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
